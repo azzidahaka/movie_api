fac26eb5c5d39121f20bc299605eea65fb34ae0b
Azzidahaka <Azzidahaka@gmail.com>
2024-02-13 22:28:14 -0700

    updated experess to serve public files

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
 //import from packages
 const express = require('express'),
-  fs = require('fs'),
-  path = require('path'),
   morgan = require('morgan');
 
 const app = express();
-// create a write stream (in append mode)
-// a ‘log.txt’ file is created in root directory
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
-app.use(morgan('combined', { stream: accessLogStream }));
-app.use(express.static('public')); //use express to serve html
+//log to console
+app.use(morgan('common'));
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 
 //Array of movies and director
 let movies = [
-    {
-      title: "Harry Potter and the Sorcerer's Stone",
-      director: 'Chris columbus',
-    },
-    {
-      title: 'The Lord of the Rings: The Return of the King',
-      director: 'Peter Jackson',
-    },
-    {
-      title: 'Twilight',
-      director: ' Catherine Hardwicke',
-    },
-    {
-        title: 'The Dark Knight',
-        director: 'Christopher Nolan',
-      },
-    {
-        title: '12 Angry Men',
-        director: 'Sidney Lumet',
-      },
-    {
-        title: 'Schindler \'s List',
-        director: 'Steven Spielberg',
-      },
-    {
-        title: 'Pulp Fiction',
-        director: 'Quentin Tarantino',
-      },
-    {
-        title: 'The Lord of the Rings: The Fellowship of the Ring',
-        director: 'Peter Jackson',
-      },
-    {
-        title: 'The Good, the Bad and the Ugly',
-        director: 'Sergio Leone',
-      },
-    {
-        title: 'The Godfather',
-        director: 'Francis Ford Coppola',
-      },
-  ];
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    director: 'Chris columbus',
+  },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    director: 'Peter Jackson',
+  },
+  {
+    title: 'Twilight',
+    director: ' Catherine Hardwicke',
+  },
+  {
+    title: 'The Dark Knight',
+    director: 'Christopher Nolan',
+  },
+  {
+    title: '12 Angry Men',
+    director: 'Sidney Lumet',
+  },
+  {
+    title: "Schindler 's List",
+    director: 'Steven Spielberg',
+  },
+  {
+    title: 'Pulp Fiction',
+    director: 'Quentin Tarantino',
+  },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    director: 'Peter Jackson',
+  },
+  {
+    title: 'The Good, the Bad and the Ugly',
+    director: 'Sergio Leone',
+  },
+  {
+    title: 'The Godfather',
+    director: 'Francis Ford Coppola',
+  },
+];
 
 //return message when get is used at "/"
 app.get('/', (req, res) => {
@@ -61,8 +59,8 @@ app.get('/', (req, res) => {
 });
 //return movies in json format
 app.get('/movies', (req, res) => {
-    res.json(movies);
-  });
+  res.json(movies);
+});
 
 //respond with error message if a request fails
 app.use((err, req, res, next) => {
