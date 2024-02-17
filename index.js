a275b703deae66ac150a153b38fdc787eb44ca01
Azzidahaka <Azzidahaka@gmail.com>
2024-02-17 01:48:45 -0700

    Updated documnentation for endpoints

--- index.js
+++ index.js
@@ -11,44 +11,64 @@ app.use(express.static('public'));
 //Array of movies and director
 let movies = [
   {
-    title: "Harry Potter and the Sorcerer's Stone",
-    director: 'Chris columbus',
+    Title: 'Silence of the Lambs',
+    Description:
+      'A young FBI cadet must recieve the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.',
+    Director: 'Chris columbus',
   },
   {
-    title: 'The Lord of the Rings: The Return of the King',
-    director: 'Peter Jackson',
+    Title: 'The Shawshank Redemption',
+    Description:
+      'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
+    Director: 'Frank Darabont',
   },
   {
-    title: 'Twilight',
-    director: ' Catherine Hardwicke',
+    Title: 'The Godfather',
+    Description:
+      'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+    Director: 'Francis Ford Coppola',
   },
   {
-    title: 'The Dark Knight',
-    director: 'Christopher Nolan',
+    Title: 'The Dark Knight',
+    Description:
+      'Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    Director: 'Christopher Nolan',
   },
   {
-    title: '12 Angry Men',
-    director: 'Sidney Lumet',
+    Title: '12 Angry Men',
+    Description:
+      'The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully consider the evidence before jumping to a hasty verdict.',
+    Director: 'Sidney Lumet',
   },
   {
-    title: "Schindler 's List",
-    director: 'Steven Spielberg',
+    Title: "Schindler 's List",
+    Description:
+      'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.',
+    Director: 'Steven Spielberg',
   },
   {
-    title: 'Pulp Fiction',
-    director: 'Quentin Tarantino',
+    Title: 'The Lord of the Rings: The Return of the King',
+    Description:
+      "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
+    Director: 'Peter jackson',
   },
   {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    director: 'Peter Jackson',
+    Title: 'Pulp Fiction',
+    Description:
+      'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+    Director: 'Quentin Tarantino',
   },
   {
-    title: 'The Good, the Bad and the Ugly',
-    director: 'Sergio Leone',
+    Title: 'The Lord of the Rings: The Fellowship of the Ring',
+    Description:
+      'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+    Director: 'Peter Jackson',
   },
   {
-    title: 'The Godfather',
-    director: 'Francis Ford Coppola',
+    Title: 'The Good, the Bad and the Ugly',
+    Description:
+      'A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.',
+    Director: 'Sergio Leone',
   },
 ];
 
@@ -64,9 +84,41 @@ app.get('/movies', (req, res) => {
 app.get('/movies/:title', (req, res) => {
   res.json(
     movies.find((movie) => {
-      return movie.title === req.params.title;
+      return movie.Title === req.params.title;
     })
   );
+});
+// Gets the data about a single movie, by name
+app.get('/movies/:title', (req, res) => {
+  res.json(
+    movies.find((movie) => {
+      return movie.Title === req.params.title;
+    })
+  );
+});
+// Adds data for a new movie to our list of movies.
+app.post('/movies', (req, res) => {
+  let newMovie = req.body;
+  if (!newMovie.Title) {
+    const message = 'Missing title in request body';
+    res.status(400).send(message);
+  } else {
+    newMovie.id = uuid.v4();
+    movies.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
+// Deletes a movie from list by ID
+app.delete('/movies/:id', (req, res) => {
+  let movie = movies.find((movie) => {
+    return movie.id === req.params.id;
+  });
+  if (movie) {
+    movies = movies.filter((obj) => {
+      return obj.id !== req.params.id;
+    });
+    res.status(201).send('movie ' + req.params.id + ' was deleted.');
+  }
 });
 //respond with error message if a request fails
 app.use((err, req, res, next) => {
