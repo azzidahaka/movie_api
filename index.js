7d318d7744680abecd3c6a0bd3e05ac716f54a38
Azzidahaka <Azzidahaka@gmail.com>
2024-02-20 22:18:22 -0700

    Added function to get all movies, movies by title and search genre by name

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //import from packages
 const express = require('express'),
+  mongoose = require('mongoose'),
+  Models = require('./models.js'),
   morgan = require('morgan');
 
+mongoose.connect('mongodb://localhost:27017/cfDB');
 const app = express();
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
 //log to console
 app.use(morgan('common'));
 // Serve static files from the 'public' directory
@@ -13,16 +20,25 @@ app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
 });
 //return movies in json format
-app.get('/movies', (req, res) => {
-  res.json(movies);
-});
-// Gets the data about a single movie, by name
-app.get('/movies/:title', (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.Title === req.params.title;
+app.get('/movies', async (req, res) => {
+  await Movies.find()
+    .then((movie) => {
+      res.json(movie);
     })
-  );
+    .catch((err) => {
+      consol.error(err);
+      res.status(500).send('Error:' + err);
+    });
+});
+//Gets the data about a movie, by name
+app.get('/movies/:title', async (req, res) => {
+  await Movies.find({Title:req.params.title}).then((movie) => {
+      res.json(movie);
+    }) .catch((err) => {
+      consol.error(err);
+      res.status(500).send('Error:' + err);
+    });
+
 });
 // Gets the data about a genre (description) by name
 app.get('/movies/genre/:genreName', async (req, res) => {
